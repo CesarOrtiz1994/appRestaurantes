8caf0cac6617237d4abddba52386df18ca5e6a23
zzALFREDzz\Uriel <urielbotelloresendiz@gmail.com>
2023-11-25 18:48:39 -0600

    keys appi google

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,11 +1,11 @@
 ﻿export const ENV = {
     API_GOOGLE_PLACES: {
-        CONF_KEY: "AIzaSyA1z4pNGYp7vCyEdzQ_y7tpI4zAapVrH_U",
+        CONF_KEY: "API_KEY_GOOGLE_PLACES",
         CONF_LANGUAGE: "es",
         CONF_COMPONENTS: 'country:MX',
         CONF_TYPES: 'restaurant',
     },
     API_GOOGLE_MAPS: {
-        KEY: "AIzaSyCSDuWHGWOw2pAMIKooMQ5TOLQhkz9u9FM"
+        KEY: "API_KEY_GOOGLE_MAPS"
     }
 }
